fc84a747db35ef9081a9895ae88ad1323d6bd814
Vladimir <andiwillfly@gmail.com>
2021-09-24 17:15:05 +0300

    dd

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// TOKEN:  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
-// git push https://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@github.com/andiwilflly/placer_client.git
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
